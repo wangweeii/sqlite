b63330aebeb64260df14f771cab25b117caa69ae
ww <ww@veso.me>
2021-04-25 22:53:01 +0800

    解析SQL语句

--- parser.c
+++ parser.c
@@ -2,4 +2,32 @@
 // Created by wangwei on 04/25.
 //
 
+#include <string.h>
+#include <stdio.h>
 #include "parser.h"
+#include "input.h"
+
+// 将输入的字符串解析为SQL语句
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+        // 先判断是不是insert语句
+        if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+        {
+                statement->type = STATEMENT_INSERT;
+                int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+                                           statement->row_to_insert.username, statement->row_to_insert.email);
+                if (args_assigned < 3)
+                {
+                        return PREPARE_SYNTAX_ERROR;
+                }
+                return PREPARE_SUCCESS;
+        }
+        // 再判断是不是select语句
+        if (strncmp(input_buffer->buffer, "select", 6) == 0)
+        {
+                statement->type = STATEMENT_SELECT;
+                return PREPARE_SUCCESS;
+        }
+        // 返回未知语句错误
+        return PREPARE_UNRECOGNIZED_STATEMENT;
+}
--- parser.h
+++ parser.h
@@ -5,4 +5,41 @@
 #ifndef SQLITE_PARSER_H
 #define SQLITE_PARSER_H
 
+#include <stdint.h>
+#include "input.h"
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+// 语句类型：insert, select
+typedef enum
+{
+        STATEMENT_INSERT,
+        STATEMENT_SELECT
+} StatementType;
+
+typedef struct
+{
+        uint32_t id; // 要插入的ID
+        char username[COLUMN_USERNAME_SIZE]; // 要插入的列名
+        char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+// SQL语句
+typedef struct
+{
+        StatementType type;
+        Row row_to_insert;
+} Statement;
+
+// 解析SQL语句的返回值
+typedef enum
+{
+        PREPARE_SUCCESS,
+        PREPARE_SYNTAX_ERROR,
+        PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement);
+
 #endif //SQLITE_PARSER_H
