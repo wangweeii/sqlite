97f79c652979b4c689f0284d467c1254de252b89
ww <ww@veso.me>
2021-05-08 00:03:52 +0800

    优化内存复制函数

--- row.cc
+++ row.cc
@@ -6,16 +6,10 @@
 
 void serialize_row(Row *source, Row *destination)
 {
-        memcpy(destination, source, ROW_SIZE);
-        // memcpy((void *)destination + ID_OFFSET, &(source->id), ID_SIZE);
-        // memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-        // memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+        memcpy(destination, source, sizeof(Row));
 }
 
 void deserialize_row(Row *source, Row *destination)
 {
-        memcpy(destination, source, ROW_SIZE);
-        // memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
-        // memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
-        // memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+        memcpy(destination, source, sizeof(Row));
 }
