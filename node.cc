c8c7d9156c75382daa513b88b161ee88130bbb43
ww <ww@veso.me>
2021-05-10 20:14:49 +0800

    增加部分函数

--- node.cc
+++ node.cc
@@ -29,9 +29,27 @@ void *node_parent(void *node)
         return ((CommonNodeHeader *) node)->parent;
 }
 
+// 读取叶子结点中共有多少个Cell
+uint32_t leaf_node_cell_nums(void *node)
+{
+        return ((LeafNodeHeader *) node)->cell_nums;
+}
+
+// 修改叶子结点中的Cell数量
+void leaf_node_cell_nums(void *node, uint32_t num)
+{
+        ((LeafNodeHeader *) node)->cell_nums = num;
+}
+
+// 获取叶子结点中某一个键值对的指针
+Cell *leaf_node_cell(void *node, uint32_t cell_num)
+{
+        return ((Cell *) ((LeafNodeHeader *) node + 1)) + cell_num;
+}
+
 void initialize_leaf_node(void *node)
 {
         set_node_type(node, NODE_LEAF);
         set_node_root(node, true);
-        ((LeafNodeHeader *) node)->cell_num = 0;
+        ((LeafNodeHeader *) node)->cell_nums = 0;
 }
--- node.h
+++ node.h
@@ -10,30 +10,17 @@
 
 struct Row;
 
+struct Cell
+{
+        uint32_t key;
+        Row      row;
+};
+
 enum NodeType : uint8_t
 {
         NODE_INTERNAL,
         NODE_LEAF
 };
-
-// Common Node Header Layout
-const uint32_t NODE_TYPE_OFFSET        = 0;
-const uint32_t NODE_TYPE_SIZE          = sizeof(uint8_t);
-const uint32_t IS_ROOT_OFFSET          = NODE_TYPE_SIZE;
-const uint32_t IS_ROOT_SIZE            = sizeof(uint8_t);
-const uint32_t PARENT_POINTER_OFFSET   = IS_ROOT_OFFSET + IS_ROOT_SIZE;
-const uint32_t PARENT_POINTER_SIZE     = sizeof(uint64_t);
-const uint8_t  COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
-
-// Leaf Node Header Layout
-const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
-const uint32_t LEAF_NODE_NUM_CELLS_SIZE   = sizeof(uint32_t);
-const uint32_t LEAF_NODE_HEADER_SIZE      = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
-
-// Leaf Node body Layout
-const uint32_t LEAF_NODE_KEY_SIZE   = sizeof(uint32_t);
-const uint32_t LEAF_NODE_KEY_OFFSET = 0;
-const uint32_t LEAF_VALUE_SIZE      = sizeof(Row);
 
 struct CommonNodeHeader
 {
@@ -54,7 +41,7 @@ struct InternalNodeHeader
 struct LeafNodeHeader
 {
         CommonNodeHeader common_header;
-        uint32_t         cell_num;
+        uint32_t         cell_nums;
 };
 
 NodeType get_node_type(void *node);
@@ -66,6 +53,15 @@ bool is_node_root(void *node);
 void set_node_root(void *node, bool is_root);
 
 void *node_parent(void *node);
+
+// 读取叶子结点中共有多少个Cell
+uint32_t leaf_node_cell_nums(void *node);
+
+// 修改叶子结点中的Cell数量
+void leaf_node_cell_nums(void *node, uint32_t num);
+
+// 获取叶子结点中某一个键值对的指针
+Cell *leaf_node_cell(void *node, uint32_t cell_num);
 
 void initialize_leaf_node(void *node);
 
